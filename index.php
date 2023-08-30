6603ed4e40c22b5f0cd1f3c431c74ad7a4d6cc13
Ben Harwood <titanharwood2016@gmail.com>
2023-08-30 13:24:28 -0400

    sandbox

--- index.php
+++ index.php
@@ -3,17 +3,27 @@
 
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB 250</title>
-    <link rel="stylesheet" href="css/styles.css">
   </head>
 
   <body>
     <h1>Web 250</h1>
+    <h2>asgn-00</h2>
     <ul>
       <li><a href="asgn-00a\event.php">Event</a></li>
       <li><a href="asgn-00a\fizz-buzz.php">Fizz-buzz</a></li>
       <li><a href="asgn-00a\flip.php">Flip</a></li>
+    </ul>
+
+    <h2>asgn-01
+      <ul>
+        <li><a href=""></a></li>
+      </ul>
+    </h2>
+
+    <h2>sandbox</h2>
+    <ul>
+      <li><a href="sandbox/cookie.php">test</a></li>
     </ul>
   </body>
 
---
+++ sandbox/cookie.php
@@ -0,0 +1,30 @@
+<?php
+  class RoundCookie {
+    var $weight;
+    var $color;
+    var $icing;
+
+    function decorate() {
+      return $this->icing;
+    }
+
+    function consume() {
+
+    }
+  }
+
+  //create a new RoundCookie object (instantiation)
+  $cookie1 = new RoundCookie;
+  $cookie1->weight = "2 oz,";
+  $cookie1->color = "green";
+  $cookie1->icing = "cream cheese icing";
+
+  $cookie2 = new RoundCookie;
+  $cookie2->weight = "24 oz,";
+  $cookie2->color = "blue";
+  $cookie2->icing = "no";
+
+
+ echo "my first cookie is ".$cookie1->weight. "it is ".$cookie1->color."it has ".$cookie1->icing.".<br>";
+ echo "my second cookie is ".$cookie2->weight."it is ".$cookie2->color."it has ".$cookie2->icing."icing."
+?>
