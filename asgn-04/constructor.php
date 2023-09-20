13ba2988d68d202ee798f46e8ca7eb2ef5bc0fd4
Ben Harwood <titanharwood2016@gmail.com>
2023-09-20 13:06:06 -0400

    started asgn-04

--- asgn-04/constructor.php
+++ asgn-04/constructor.php
@@ -1,1 +1,11 @@
+<?php 
+  class Bird {
+    public $commonName;
+    public $latinName;
 
+    public function __construct($commonName, $latinName) {
+      $this->commonName = $commonName;
+      $this->latinName = $latinName;
+    }
+  }
+?>
