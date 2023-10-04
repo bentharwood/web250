0785b40ae4b1b6ef0f6e812e1292acd1f1731000
Ben Harwood <titanharwood2016@gmail.com>
2023-10-04 13:20:11 -0400

    Css

--- index.php
+++ index.php
@@ -4,38 +4,103 @@
 <head>
   <meta charset="utf-8">
   <title>WEB 250</title>
+  <style>
+     @import url('https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap');
+    html {
+      background-color: #081326;
+      color: #fff;
+      font: 20px 'Crimson Text', serif;
+    }
+
+    body {
+      margin: 1rem;
+    }
+
+    li {
+      list-style: none;
+    }
+
+    ul,li {
+      margin: 0;
+      padding: 0;
+    }
+
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
+
+    #wrapper {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+    }
+
+    h1 {
+      grid-column: 1/ -1;
+      text-align: center;
+    } 
+
+    #container {
+      border: 1px solid #000;
+      text-align: center;
+      margin: .5rem;
+      padding: 0.5rem;
+    }
+  </style>
 </head>
 
 <body>
-  <h1>Web 250</h1>
-  <h2>asgn-00</h2>
-  <ul>
-    <li><a href="asgn-00a\event.php">Event</a></li>
-    <li><a href="asgn-00a\fizz-buzz.php">Fizz-buzz</a></li>
-    <li><a href="asgn-00a\flip.php">Flip</a></li>
-  </ul>
-  <h2>asgn-01</h2>
-  <ul>
-    <li><a href="asgn-01b\bike challenge">Bike challenge</a></li>
-    <li><a href="asgn-01b\bird challenge">Bird challenge</a></li>
-  </ul>
-  <h2>asgn-02</h2>
-  <ul>
-    <li><a href="asgn-02a-inheritance\challenge-02-inheritence.php">challenge 02 inheritance</a></li>
-    <li><a href="asgn-02a-inheritance\challenge-03-oac.php">challenge 03 oac</a></li>
-  </ul>
-  <h2>asgn-03</h2>
-  <li><a href="asgn-03\index.php">asgn-03</a></li>
-  <h2>asgn-04</h2>
-  <ul>
-    <li><a href="asgn-04\constructor.php">asgn-04</a></li>
-    <li><a href="asgn-04\constructor-arguments.php">asgn-04-arguments</a></li>
-    <li><a href="asgn-04\autoload.php">autoload</a></li>
-  </ul>
-  <h2>asgn-05</h2>
-  <ul>
-    <li><a href="asgn-05\public\index.php">asgn-05</a></li>
-  </ul>
+  <div id="wrapper">
+    <h1>Web 250</h1>
+    <div id="container">
+      <h2>asgn-00</h2>
+      <ul>
+        <li><a href="asgn-00a\event.php">Event</a></li>
+        <li><a href="asgn-00a\fizz-buzz.php">Fizz-buzz</a></li>
+        <li><a href="asgn-00a\flip.php">Flip</a></li>
+      </ul>
+    </div>
+    <div id="container">
+      <h2>asgn-01</h2>
+      <ul>
+        <li><a href="asgn-01b\bike challenge">Bike challenge</a></li>
+        <li><a href="asgn-01b\bird challenge">Bird challenge</a></li>
+      </ul>
+    </div>
+    <div id="container">
+      <h2>asgn-02</h2>
+      <ul>
+        <li><a href="asgn-02a-inheritance\challenge-02-inheritence.php">challenge 02 inheritance</a></li>
+        <li><a href="asgn-02a-inheritance\challenge-03-oac.php">challenge 03 oac</a></li>
+      </ul>
+    </div>
+    <div id="container">
+      <h2>asgn-03</h2>
+      <ul>
+        <li><a href="asgn-03\index.php">asgn-03</a></li>
+      </ul>
+    </div>
+    <div id="container">
+      <h2>asgn-04</h2>
+      <ul>
+        <li><a href="asgn-04\constructor.php">asgn-04</a></li>
+        <li><a href="asgn-04\constructor-arguments.php">asgn-04-arguments</a></li>
+        <li><a href="asgn-04\autoload.php">autoload</a></li>
+    </div>
+    <div id="container">
+      </ul>
+      <h2>asgn-05</h2>
+      <ul>
+        <li><a href="asgn-05\public\index.php">asgn-05</a></li>
+      </ul>
+    </div>
+    <div id="container">
+      <h2>asgn-06</h2>
+      <ul>
+        <li><a href="asgn-06\public\index.php">asgn-06</a></li>
+      </ul>
+    </div>
+  </div>
 </body>
 
 </html>
