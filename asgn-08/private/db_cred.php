c64c58474684da170c3f91c3f6d8246c8d7343a7
bentharwood <bentharwood@students.abtech.edu>
2023-10-25 17:39:50 -0400

    resolving access to database

--- asgn-08/private/db_cred.php
+++ asgn-08/private/db_cred.php
@@ -1,6 +1,6 @@
 <?php 
   define("DB_SERVER","localhost");
-  define("DB_USER","sabirdsUser");
+  define("DB_USER","sabirdsuser");
   define("DB_PASS","cassowary");
   define("DB_NAME","sabirds");
 ?>
